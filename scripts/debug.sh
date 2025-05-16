a5c72f091bbef04d3c2e54d7332cbf68b86948bd
RuchchaSD <ruchithsd@gmail.com>
2025-05-17 00:38:22 +0530

    .

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -33,7 +33,7 @@ unset DEBUG_MonocularInitializationShowMatchedCoords
 unset USE_ORB
 
 # Change here - uncomment the debug flags you want to set
-# export DEBUG_FEAT=1
+export DEBUG_FEAT=1
 # export DEBUG_SLAM=1
 # export SKIP_SLAM=1  # Set this to skip SLAM initialization and processing (for feature extraction only)
 
